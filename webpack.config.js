c44302aa4d2923aa98227a4caeeacc5aa8d47a8c
Ariel Jimenez <ajimenez@intellisys.com.do>
2017-09-15 00:25:51 -0400

    Add webpack and compiles :D

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: [
-        './app/index.js'
-    ],
-    module: {
-        rules: []
-    },
-}
+  entry : "./app/index.js",
+  output: {
+    path:  path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader'},
+      { test: /\.css$/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  plugins: [ new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
+};
